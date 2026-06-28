eb59fcd37e915dc74bbd12cf00f179afebed6185
sravankumar0405 <sammetasravan2002@gmail.com>
2026-06-28 23:26:25 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div className="h-screen flex items-center justify-center bg-slate-900">
