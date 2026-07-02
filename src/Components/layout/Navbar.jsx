5f73a6bcffb4a9fef504dad6b952b08223aca31d
sravankumar0405 <sammetasravan2002@gmail.com>
2026-07-02 10:26:12 +0530

    commit changes

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -1,8 +1,47 @@
+import { Link } from "react-router-dom";
+import myLogo from "../../assets/images/mylogo.png";
+
+
 function Navbar() {
-    return(
-        <div>
-            <h1>Navbar</h1>
-        </div>
+    return (
+            <header className="border-b border-gray-200">
+               <div className="max-w-7xl mx-auto px-6">
+                 <nav className="flex justify-between items-center px-8 h-20">
+
+                    <div>
+                        {/* <Link to="/">BuzzBuy</Link> */}
+                        <img src={myLogo} alt="logo" className="w-[110px] h-auto"/>
+                        
+                    </div>
+                    
+                    <ul className="flex gap-8">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/categories">Categories</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+
+                    <div className="flex gap-8">
+                        Search
+                        Cart 
+                        Login 
+                        Register
+                    </div>
+
+                </nav>
+               </div>
+            </header>
     )
 }
 
